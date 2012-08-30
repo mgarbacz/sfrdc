947f3f18e84eb9319abe3e391097f1913fa10868
mgarbacz <mich.garbacz@gmail.com>
2012-08-30 14:06:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfrdc::Application.config.secret_token = '6b1a73c5744719d44a63be1fad10050998e24576e2531e494399218f5aec3a01330633f698680d00f0782dbd22ac6aaa6926ef9547991cdf1fd80699575cb2bb'
