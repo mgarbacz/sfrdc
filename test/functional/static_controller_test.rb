b7dc72c5dd19d65005301bdaebe2a65190940bfc
mgarbacz <mich.garbacz@gmail.com>
2012-09-04 15:53:52 -0500

    Tests created for rendering of static pages

--- test/functional/static_controller_test.rb
+++ test/functional/static_controller_test.rb
@@ -1,6 +1,42 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about_us" do
+    get :about_us
+    assert_response :success
+  end
+
+  test "should get opportunities" do
+    get :opportunities
+    assert_response :success
+  end
+
+  test "should get interns_and_staff" do
+    get :interns_and_staff
+    assert_response :success
+  end
+
+  test "should get benefits" do
+    get :benefits
+    assert_response :success
+  end
+
+  test "should get partnerships" do
+    get :partnerships
+    assert_response :success
+  end
+
+  test "should get contact_us" do
+    get :contact_us
+    assert_response :success
+  end
+
   test "should get admin" do
     get :admin
     assert_response :success
