4719adc66b3672a01049493499b2f4b9ba0cd995
mgarbacz <mich.garbacz@gmail.com>
2012-09-04 15:54:31 -0500

    Added views, actions, and routes for static pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,34 @@
 class StaticController < ApplicationController
+
+  # Home page
+  def home
+  end
+
+  # About Us page
+  def about_us
+  end
+
+  # Opportunities page
+  def opportunities
+  end
+
+  # Interns & Staff page
+  def interns_and_staff
+  end
+
+  # Benefits page
+  def benefits
+  end
+
+  # Partnerships page
+  def partnerships
+  end
+
+  # Contact Us page
+  def contact_us
+  end
+
+  # Administration page
   def admin
   end
 end
