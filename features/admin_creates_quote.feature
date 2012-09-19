db410a5fc54f00361dfb10f0b1d6d8e43cb0bf60
mgarbacz <mich.garbacz@gmail.com>
2012-09-19 18:33:47 -0500

    Changed test cases for quote to check for an additional quote

--- features/admin_creates_quote.feature
+++ features/admin_creates_quote.feature
@@ -1,8 +1,14 @@
 Feature: admin creates quote
   As an admin
-  I want to create a quote
+  I want to create a new quote
   So that I have a new quote
 
-  Scenario: create quote
-    When I create a quote
-    Then the quote should exist in the database
+  Scenario: add new quote when quotes already exist 
+    Given I know how many quotes I have
+    When I create a new quote
+    Then I should have one additional quote
+
+  Scenario: add new quote when no quotes exist
+    Given I have no quotes
+    When I create a new quote
+    Then I should have 1 quote
--- features/step_definitions/quote_steps.rb
+++ features/step_definitions/quote_steps.rb
@@ -1,8 +1,22 @@
-When /^I create a quote$/ do
+Given /^I know how many quotes I have$/ do
+  @number_quotes = Quote.count
+end
+
+Given /^I have no quotes$/ do
+  Quote.delete_all
+end
+
+When /^I create a new quote$/ do
   quote = Quote.new
+  quote.quote = 'Test quote'
+  quote.author = 'Quote tester'
   quote.save
 end
 
-Then /^the quote should exist in the database$/ do
-  quote = Quote.find(1)
+Then /^I should have one additional quote$/ do
+  Quote.count.should == @number_quotes + 1
+end
+
+Then /^I should have (\d+) quote$/ do |num_quotes|
+  Quote.count.should == num_quotes.to_i
 end
