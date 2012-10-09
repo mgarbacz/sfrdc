4eec2429ba8314e2b2602fddf94e796d4c4d9cf3
mgarbacz <mich.garbacz@gmail.com>
2012-10-09 15:19:01 -0500

    Modified employee avatars to be 300x300 for regular view, and added the folder where they are stored to gitignore

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,5 @@ class Employee < ActiveRecord::Base
   attr_accessible :name, :employee_type, :department, :bio, :avatar
   validates :name, :employee_type, :department, :presence => true
   has_attached_file :avatar, 
-                    :styles => { :regular => "400x400>", :thumb => "100x100>" }
+                    :styles => { :regular => "300x300#", :thumb => "100x100#" }
 end
