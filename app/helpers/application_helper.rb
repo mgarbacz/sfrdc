438c3f38a6067fc3d08d21800ad91f5345584db1
mgarbacz <mich.garbacz@gmail.com>
2012-11-19 17:07:02 -0600

    Updated flash messages to include a note in strong font-style based on level

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   # Returns the Twitter Bootstrap class for a particular message level
   def get_msg_class(msg_level)
     case msg_level
-      when :notice  then 'alert-success'
-      when :error   then 'alert-error'
-      when :alert   then 'alert-info'
+      when :notice  then {css: 'alert-success', note: 'Well done!'}
+      when :error   then {css: 'alert-error',   note: 'Oh snap!'}
+      when :alert   then {css: 'alert-info',    note: 'Heads up!'}
     end
   end
 end
