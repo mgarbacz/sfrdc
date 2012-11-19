c8a13021b94f436793033e304d96773c88ae4a31
mgarbacz <mich.garbacz@gmail.com>
2012-11-19 16:54:35 -0600

    Moved flash messages to their own partial and hooked them up with bootstrap styling via a helper to translate flash messages to bootstrap alert classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Returns the Twitter Bootstrap class for a particular message level
+  def get_msg_class(msg_level)
+    case msg_level
+      when :notice  then 'alert-success'
+      when :error   then 'alert-error'
+      when :alert   then 'alert-info'
+    end
+  end
 end
