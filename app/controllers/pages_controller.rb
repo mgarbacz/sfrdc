69dfb2db5abaa7a43b327c6754427144df0789fb
mgarbacz <mich.garbacz@gmail.com>
2013-02-27 19:56:03 -0600

    Added add_component_button to all pages that require it when admin is logged in, passing page value in as well

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,37 @@
 class PagesController < ApplicationController
-before_filter :authenticate_admin!, :only => :admin
+  before_filter :authenticate_admin!, :only => :admin
+
+  # Setting the page automatically
+  def initialize_component(page)
+    if admin_signed_in?
+      @component = Component.new
+      @component.page = page
+    end
+  end
 
   # Home page
   def home
-    @component = Component.new
-    @component.page = 'Home'
+    initialize_component('Home')
   end
 
   # About Us page
   def about_us
+    initialize_component('About Us')
   end
 
   # Benefits page
   def benefits
+    initialize_component('Benefits')
   end
 
   # Partnerships page
   def partnerships
+    initialize_component('Partnerships')
   end
 
   # Contact Us page
   def contact_us
+    initialize_component('Contact Us')
   end
 
   # Administration page
